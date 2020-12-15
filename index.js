ee55cacecde0c412aab7fb9a24967859e4e461c6
kleitonalbuquerque <kleiton2102@gmail.com>
2020-12-15 19:05:15 -0300

    Instalando o bootstrap

--- index.js
+++ index.js
@@ -6,46 +6,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Routes
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-    var produtos = [
-        { 
-            nome: "Doritos",  
-            preco: 5.99
-        },
-        {
-            nome: "Coca-Cola",
-            preco: 3.99
-        },
-        {
-            nome: "Leite",
-            preco: 2.99
-        },
-        {
-            nome: "Alcatra",
-            preco: 30.99
-        },
-        {
-            nome: "Red Bull",
-            preco: 8.99
-        },
-        {
-            nome: "Nescau",
-            preco: 5.99
-        }
-    ];
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "KLMTech",
-        msg: exibirMsg,
-        produtos: produtos
-    })
+app.get("/", (req, res) => {
+    res.render("index")
 });
-
 
 // Server
 app.listen(8000, (erro) => {
